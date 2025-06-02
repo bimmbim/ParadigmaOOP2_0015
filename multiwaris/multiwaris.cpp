bec8cfaafc8dd247520f49afc88d820ad5b311e8
bimmbim <bimabaraja76@gmail.com>
2025-06-02 10:44:45 +0700

    tambah kelas pekerja turunan dari orang

--- multiwaris/multiwaris.cpp
+++ multiwaris/multiwaris.cpp
@@ -11,7 +11,13 @@ class orang {
             }
 };
 
-
+class pekerja : virtual public orang {
+    public:
+        pekerja(int pUmur) :
+        orang(pUmur) {
+            cout << "pekerja dibuat\n" << endl;
+        }
+};
 
 
 
