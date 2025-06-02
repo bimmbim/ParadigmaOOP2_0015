90c319a356d9fab90a8cb436d98dc74cf1f1e4f4
bimmbim <bimabaraja76@gmail.com>
2025-06-02 10:51:34 +0700

    objek derivedClass dan panggil method perkenalan di main()

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -14,3 +14,11 @@ class derivedClass : public baseClass {
             cout << "Hallo saya Function dari derived Class";
         }
 };
+
+int main() {
+    derivedClass a;
+
+    a.perkenalan();
+
+    return 0;
+}
