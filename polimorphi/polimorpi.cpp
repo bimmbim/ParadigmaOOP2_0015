75f52e25b4b34affd03a554beaa180f70ebe0da3
bimmbim <bimabaraja76@gmail.com>
2025-06-02 11:17:07 +0700

    Implementasi metode 'pesan' di kelas joko dan lia, serta uji polimorfisme di main()

--- polimorphi/polimorpi.cpp
+++ polimorphi/polimorpi.cpp
@@ -10,4 +10,30 @@ class seseorang {
 
     };
 
+class joko :public seseorang {
+    public:
+    void pesan() {
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia :public seseorang {
+    public:
+    void pesan() {
+        cout << "Pesan dari lia" << endl;
+    }
+};
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+
+    return 0;
+}
 
